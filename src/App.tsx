21b1ad0af1bfd1c27c81b8d53d5b7a0d98d734c8
City of Elseways <80192890+elseways@users.noreply.github.com>
2021-03-09 20:50:25 -0800

    initial commit - basic header

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="mainPage">
+      <header className="header">
+        City of Elseways Map
+      </header>
+      <main>
+        <div className="map">
+
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
